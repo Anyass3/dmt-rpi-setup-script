42a8078134de76a7eecf3cd4f1f4f7f44bcd6873
Anyass3 <nyassabu@gmail.com>
2021-10-27 23:23:19 +0000

    fixed browser-auto-start-script

--- adjust-browser-auto-start-script.py
+++ adjust-browser-auto-start-script.py
@@ -1,7 +1,7 @@
 
 import os
 
-path=os.path.join(os.environ.get('HOME'),'.config/lxsession/LXDE-pi/autostart')
+path=os.path.join('/home/pi','.config/lxsession/LXDE-pi/autostart')
 string="""
 @/usr/bin/chromium-browser --incognito --start-maximized --start-fullscreen --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --overscroll-history-navigation=0 --disk-cache-dir=/dev/null --disable-pinch --force-tablet-mode --tablet-ui --kiosk http://localhost
 
