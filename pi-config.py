61eb8412d34611d47c0824f4cce8f5320f81a5ca
Abu <42287254+Anyass3@users.noreply.github.com>
2021-10-27 23:27:17 +0000

    Update pi-config.py

--- pi-config.py
+++ pi-config.py
@@ -5,6 +5,6 @@ print();
 config=input(Fore.BLUE+'Press Enter to continue to raspi-config or any other key to skip it for now: ')
 
 if config:
-    print('You can setup again with'+Fore.MAGENTA+' raspi-config')
+    print('You can setup later with'+Fore.MAGENTA+' raspi-config')
 else:
     os.system('raspi-config')
