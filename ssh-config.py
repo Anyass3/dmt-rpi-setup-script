e422e1de5584a2053ec0c60462169b7d2d48103e
Anyass3 <nyassabu@gmail.com>
2021-10-28 06:36:56 +0000

    fix ssh-conf

--- ssh-config.py
+++ ssh-config.py
@@ -1,7 +1,7 @@
 import re
 with open('/etc/ssh/sshd_config') as f:
     content=f.read()
-    content = re.sub(r'#PermitRootLogin Yes','PermitRootLogin Yes' , content)
+    content = re.sub(r'#PermitRootLogin.*\n','PermitRootLogin Yes\n' , content)
     content = re.sub(r'#UseDNS no','UseDNS no' , content)
 with open('/etc/ssh/sshd_config','w') as f:
     f.write(content)
