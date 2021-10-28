a182f8a084fddde4454738125f11d0844f0b4828
Anyass3 <nyassabu@gmail.com>
2021-10-28 10:50:56 +0000

    updated

--- public_key.py
+++ public_key.py
@@ -3,6 +3,6 @@ import os
 
 path=os.path.join(os.environ.get('HOME'),'.ssh/authorized_keys')
 with open(path,'a') as ak:
-  p_key=input(Fore.BLUE+'Enter public authorized key: ')
+  p_key=input(Fore.BLUE+'Enter public authorized key: ').trim()
   if p_key:
       ak.write('\n\n'+p_key)
